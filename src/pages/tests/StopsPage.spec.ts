bf062e52482939e63d69745bfc28b22377c47c1c
Kamil Golec <kumalgfilms@gmail.com>
2024-11-06 17:07:12 +0100

    stops page tests

---
+++ src/pages/tests/StopsPage.spec.ts
@@ -0,0 +1,41 @@
+import { shallowMount } from '@vue/test-utils';
+import { createStore } from 'vuex';
+
+import { key } from '@/store';
+
+import StopsPage from '@/pages/StopsPage.vue';
+import CardList from '@/components/CardList/CardList.vue';
+
+describe('StopsPage', () => {
+    const STOPS = ['stop1', 'stop2', 'stop3'];
+
+    const StopsPageFactory = () => {
+        const store = createStore({
+            getters: {
+                stops: () => STOPS
+            }
+        });
+        return shallowMount(StopsPage, {
+            global: {
+                provide: {
+                    [key as unknown as string]: store
+                },
+                stubs: {
+                    CardList
+                }
+            }
+        });
+    };
+
+    it('renders proper html markup', () => {
+        const wrapper = StopsPageFactory();
+
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders correct stops list', () => {
+        const wrapper = StopsPageFactory();
+
+        expect(wrapper.findComponent(CardList).props()['items']).toEqual(STOPS);
+    });
+});
